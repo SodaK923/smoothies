125bbdeadc22abef909f309344930cc24aa01fd4
Soda <k.sua923@gmail.com>
2025-05-19 02:46:15 +0900

    a href

--- src/App.js
+++ src/App.js
@@ -9,8 +9,11 @@ function App() {
     <BrowserRouter basename={process.env.PUBLIC_URL}>
     <nav>
       <h1>Supa Smoothies</h1>
+      {/* Link to: 새로고침X a href: 새로고침O */}
       <Link to="/">Home</Link>
       <Link to="/create">Create New Smoothie</Link>
+      <a href="https://www.daum.net">다음</a>
+      <a href="/smoothies">홈</a>
     </nav>
     <Routes>
       <Route path="/" element={<Home />} />
